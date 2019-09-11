18c6dfca5ec4c7913bf3f3d060fa0f90712abc4b
Andrew Anderson <aanderso@tcd.ie>
2019-09-11 16:27:42 +0100

    More messing

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This project is a modified version of https://github.com/ypwhs/captcha_break/
 
-To install the required dependencies, you can use `pip --user install`
+To install the required dependencies, you can use `pip install --user`
 
 The `--user` is important if you are running on a shared machine (i.e. a server) where you do not have root.
--- generate.py
+++ generate.py
@@ -6,21 +6,19 @@ import numpy as np
 import random
 import string
 import tensorflow as tf
-import tensorflow.keras.backend as K
+import tensorflow.keras.backend
 from tensorflow.keras.utils import Sequence
-
-%matplotlib inline
-%config InlineBackend.figure_format = 'retina'
+import cv2
 
 characters = string.digits + string.ascii_uppercase
 print(characters)
 
 width, height, n_len, n_class = 128, 64, 4, len(characters)
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
-sess = tf.Session(config=config)
-K.set_session(sess)
+config = tf.compat.v1.ConfigProto()
+# config.gpu_options.allow_growth=True
+sess = tf.compat.v1.Session(config=config)
+# tf.compat.v1.keras.backend.set_session(sess)
 
 class CaptchaSequence(Sequence):
     def __init__(self, characters, batch_size, steps, n_len=4, width=128, height=64):
@@ -51,7 +49,18 @@ def decode(y):
     y = np.argmax(np.array(y), axis=2)[:,0]
     return ''.join([characters[x] for x in y])
 
-data = CaptchaSequence(characters, batch_size=1, steps=1)
-X, y = data[0]
-plt.imshow(X[0])
-plt.title(decode(y))
+data = CaptchaSequence(characters, batch_size=10, steps=2)
+# X, y = data[0]
+# imgplot = plt.imshow(X[0])
+# plt.title(decode(y))
+# plt.show()
+
+for ix in range(len(data)):
+  X, y = data[ix]
+  image = X[0]
+  print(image.shape)
+  plt.imshow(image)
+  plt.show()
+  image_transposed = np.uint8(np.transpose(image, (2, 0, 1)))
+  print(image_transposed.shape)
+  cv2.imwrite("data/captcha-"+str(decode(y))+".png", image_transposed)
