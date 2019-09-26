e666ad8f5504ad5cfbf72a4df46124221b68217c
Andrew Anderson <aanderso@tcd.ie>
2019-09-26 15:33:43 +0100

    Add challege creation script and clean up captcha generation script

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Required dependencies: python-captcha, opencv, python-tensorflow (CPU or GPU)
 ## Generating captchas
 
 ```
-./generate.py --width 128 --height 64 --length 4 --symbols symbols.txt --count 3200 --scramble --output-dir test
+./generate.py --width 128 --height 64 --length 4 --symbols symbols.txt --count 3200 --output-dir test
 ```
 
 This generates 3200 128x64 pixel captchas with 4 symbols per captcha, using the
@@ -14,32 +14,10 @@ set of symbols in the `symbols.txt` file. The captchas are stored in the folder
 `test`, which is created if it doesn't exist. The names of the captcha images
 are scrambled.
 
-Without the `--scramble` option, the name of the image is the captcha text.
-
 To train and validate a neural network, we need two sets of data: a big
 training set, and a smaller validation set. The network is trained on the
 training set, and tested on the validation set, so it is very important that
 there are no images that are in both sets.
-
-To generate the training data, the "ground truth" classification for each
-training example image must be known. This means that for training, the names
-of the captchas *cannot* be scrambled, because otherwise the training process
-has no way to check if the answer from the CNN for some captcha is right or
-wrong! Make sure not to use the `--scramble` option when generating the
-training or validation datasets.
-
-### Generating Identical Sequences
-
-Using the `--seed` argument to the generator script, you can pass an arbitrary
-string that will be used as a random seed for captcha generation. This can be
-an integer, some text, or any other string that's representable in `utf-8`
-encoding.
-
-Setting the seed to the same value will mean the same sequence of captchas is
-produced. Note that the exact same sequence of *images* will not be produced,
-because a given captcha text can be rendered in many different ways. However
-the same sequence of captcha text samples will be produced.
-
 
 ## Training the neural network
 
--- generate.py
+++ generate.py
@@ -8,23 +8,14 @@ import cv2
 import argparse
 import captcha.image
 
-def scramble_image_name(image_name):
-    import hashlib
-    m = hashlib.sha1()
-    m.update(image_name.encode('utf-8'))
-    return m.hexdigest()
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--width', help='Width of captcha image', type=int)
     parser.add_argument('--height', help='Height of captcha image', type=int)
     parser.add_argument('--length', help='Length of captchas in characters', type=int)
     parser.add_argument('--count', help='How many captchas to generate', type=int)
-    parser.add_argument('--scramble', help='Whether to scramble image names', default=False, action='store_true')
-    parser.add_argument('--mapping', help='Where to save the scrambled to unscrambled mapping', type=str)
     parser.add_argument('--output-dir', help='Where to store the generated captchas', type=str)
     parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
-    parser.add_argument('--seed', help='Seed for the captcha generator', type=str)
     args = parser.parse_args()
 
     if args.width is None:
@@ -51,12 +42,6 @@ def main():
         print("Please specify the captcha symbols file")
         exit(1)
 
-    random_seed = None
-    if args.seed is not None:
-        random_seed = args.seed.encode('utf-8')
-
-    random.seed(random_seed)
-
     captcha_generator = captcha.image.ImageCaptcha(width=args.width, height=args.height)
 
     symbols_file = open(args.symbols, 'r')
@@ -69,31 +54,17 @@ def main():
         print("Creating output directory " + args.output_dir)
         os.makedirs(args.output_dir)
 
-    mapping = None
-    if args.mapping is not None:
-        mapping = dict()
-
     for i in range(args.count):
         captcha_text = ''.join([random.choice(captcha_symbols) for j in range(args.length)])
-        image_name_scrambled = captcha_text
-        if args.scramble:
-            image_name_scrambled = scramble_image_name(captcha_text)
-            if args.mapping is not None:
-                mapping[image_name_scrambled+'.png'] = captcha_text
-        image_path = os.path.join(args.output_dir, image_name_scrambled+'.png')
+        image_path = os.path.join(args.output_dir, captcha_text+'.png')
         if os.path.exists(image_path):
             version = 1
-            while os.path.exists(os.path.join(args.output_dir, image_name_scrambled + '_' + str(version) + '.png')):
+            while os.path.exists(os.path.join(args.output_dir, captcha_text + '_' + str(version) + '.png')):
                 version += 1
-            image_path = os.path.join(args.output_dir, image_name_scrambled + '_' + str(version) + '.png')
+            image_path = os.path.join(args.output_dir, captcha_text + '_' + str(version) + '.png')
 
         image = numpy.array(captcha_generator.generate_image(captcha_text))
         cv2.imwrite(image_path, image)
-
-    if args.mapping is not None:
-        with open(args.mapping, 'w') as mapping_file:
-            for (hashed_text, orig_text) in mapping.items():
-                mapping_file.write(hashed_text + ", " + orig_text + "\n")
 
 if __name__ == '__main__':
     main()
