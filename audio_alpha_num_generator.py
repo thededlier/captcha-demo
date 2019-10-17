03b51cc79071b47aa3f087bbfb1c432f374b05d0
Rohan Anand <thededlier@gmail.com>
2019-10-17 20:05:12 +0100

    audio single letter/digit generator

--- audio_alpha_num_generator.py
+++ audio_alpha_num_generator.py
@@ -6,7 +6,6 @@ import numpy
 import random
 import string
 import argparse
-import gtts
 import pyttsx3
 
 def main():
@@ -33,22 +32,25 @@ def main():
         print("Creating output directory " + args.output_dir)
         os.makedirs(args.output_dir)
 
-    engine = pyttsx3.init()
+    engine = pyttsx3.init(driverName='sapi5', debug=True)
 
     for i in range(len(captcha_symbols)):
-        alphanum = captcha_symbols[i]
-        audio_path = os.path.join(args.output_dir, alphanum+'.wav')
-        version = 1
-        while os.path.exists(os.path.join(args.output_dir, alphanum + '_' + str(version) + '.wav')):
-            version += 1
-        for voice in engine.getProperty('voices'):
-            audio_path = os.path.join(args.output_dir, alphanum + '_' + str(version) + '.wav')
+        for voice_speed in [100, 125, 150, 175, 200, 225, 250, 300]:
+            alphanum = captcha_symbols[i]
+            audio_path = os.path.join(args.output_dir, alphanum+'.wav')
+            version = 1
+            while os.path.exists(os.path.join(args.output_dir, alphanum + '_' + str(version) + '.wav')):
+                version += 1
+            for voice in engine.getProperty('voices'):
+                audio_path = os.path.join(args.output_dir, alphanum + '_' + str(version) + '.wav')
 
-            print(audio_path + '=' + alphanum)
+                print(audio_path + '=' + alphanum)
 
-            engine.setProperty('voice', voice.id)
-            engine.save_to_file(alphanum, audio_path)
-            version += 1
-
+                engine.setProperty('voice', voice.id)
+                engine.setProperty('rate', voice_speed)
+                engine.save_to_file(alphanum, audio_path)
+                engine.runAndWait()
+                version += 1
+    engine.stop()
 if __name__ == '__main__':
     main()
